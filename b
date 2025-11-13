454585bbb5110c3e7cbf71ca1a65ffe2fabc3e3e
Tobias Aichinger <aichinger.tobias13@gmail.com>
2025-11-13 23:09:44 +0100

    added vma and volk to make life easier

--- b
+++ b
@@ -2,7 +2,12 @@
 
 set -e
 
-g++ -I$HOME/ext/glfw/include src/main.cpp -o uve -L$HOME/ext/glfw/build/src -l:libglfw3.a 
+g++                                             \
+    -I$HOME/ext/glfw/include                    \
+    -I$HOME/ext/volk/                           \
+    -I$HOME/ext/VulkanMemoryAllocator/include   \
+    src/main.cpp -o uve                         \
+    -L$HOME/ext/glfw/build/src -l:libglfw3.a
 
 if test "$#" -ge 1 -a "$1" = "--run" ; then
     ./uve
--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,20 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#define VOLK_IMPLEMENTATION
+#include "volk.h"
+
+#define VMA_IMPLEMENTATION
+#define VMA_VULKAN_VERSION 1004000
+#include "vk_mem_alloc.h"
+
 int main(void) {
     if (!glfwInit()) return 1;
+    if (volkInitialize() != VK_SUCCESS) return 1;
+
+    uint32_t version = volkGetInstanceVersion();
+
+    std::cout << VK_VERSION_MAJOR(version) << '.' << VK_VERSION_MINOR(version) << '.' << VK_VERSION_PATCH(version) << '\n';
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow *win = glfwCreateWindow(640, 480, "uve", nullptr, nullptr);
@@ -12,6 +24,19 @@ int main(void) {
         glfwTerminate();
         return 1;
     }
+
+    //VmaAllocator allocator;
+    //VmaVulkanFunctions vulkanFunctions = {};
+    //vulkanFunctions.vkGetInstanceProcAddr = &vkGetInstanceProcAddr;
+    //vulkanFunctions.vkGetDeviceProcAddr = &vkGetDeviceProcAddr;
+    //VmaAllocatorCreateInfo allocatorCreateInfo = {
+    //    .flags = VMA_ALLOCATOR_CREATE_EXT_MEMORY_BUDGET_BIT;
+    //    .vulkanApiVersion = VK_API_VERSION_1_2;
+    //    .physicalDevice = physicalDevice;
+    //    .device = device;
+    //    .instance = instance;
+    //    .pVulkanFunctions = &vulkanFunctions;
+    //};
 
     while (!glfwWindowShouldClose(win)) {
         glfwPollEvents();
